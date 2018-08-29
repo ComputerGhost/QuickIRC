6f018e18fef0b6c351fd072cdb05d07653ad1d04
computerghost <nathan.belue@gmail.com>
2018-08-28 19:36:10 -0700

    Version string now formatted correctly.

--- src/GUI/Forms/AboutQuickIRC.vb
+++ src/GUI/Forms/AboutQuickIRC.vb
@@ -3,10 +3,12 @@
     Private Sub Me_Load() Handles Me.Load
 
         Dim app_info = My.Application.Info
+        Dim version_info = app_info.Version
+        Dim version_string = String.Format("{0}.{1}.{2}", version_info.Major, version_info.Minor, version_info.Revision)
 
         Text = "About " & app_info.Title
 
-        lblProductName.Text = String.Format("{0} Version {1}", app_info.ProductName, app_info.Version.ToString())
+        lblProductName.Text = String.Format("{0} Version {1}", app_info.ProductName, version_string)
         lblCopyright.Text = String.Format("{0} {1}", app_info.Copyright, app_info.CompanyName)
         lblDescription.Text = app_info.Description
 
