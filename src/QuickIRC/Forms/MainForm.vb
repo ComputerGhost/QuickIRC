f09378f9624e210606c9c0ca73dc8c1ae662f9a4
computerghost <nathan.belue@gmail.com>
2018-11-18 18:25:17 -0800

    Displays the server's favicon as the small form icon.

--- src/QuickIRC/Forms/MainForm.vb
+++ src/QuickIRC/Forms/MainForm.vb
@@ -1,4 +1,7 @@
 ﻿Imports System.ComponentModel
+Imports System.Net.Cache
+Imports System.Runtime.InteropServices
+Imports FaviconFetcher
 
 Public Class MainForm
 
@@ -16,6 +19,7 @@ Public Class MainForm
         ' Attach to our UI
         Me.Text = connection_info.Name
         ClientArea.AttachConnection(Connection)
+        _LoadFaviconAsync(connection_info)
 
         ' Change out some visibility now that we have a connection
         btnConnect.Visible = False
@@ -24,6 +28,13 @@ Public Class MainForm
         ' Now we start
         Connection.Connect()
 
+    End Sub
+
+    Private Sub Form_Closing() Handles Me.FormClosing
+        If Connection IsNot Nothing Then
+            Connection.Disconnect()
+            _Connection = Nothing
+        End If
     End Sub
 
     Private Sub btnConnect_Click() Handles btnConnect.Click
@@ -64,6 +75,53 @@ Public Class MainForm
 
     Private Sub AboutQuickIRCToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutQuickIRCToolStripMenuItem.Click
         AboutForm.Show()
+    End Sub
+
+#End Region
+
+#Region "Helpers"
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SendMessage(hWnd As IntPtr, Msg As UInteger, wParam As Integer, lParam As IntPtr) As IntPtr
+    End Function
+    Private Const WM_SETICON As UInteger = &H80
+    Private Const ICON_SMALL As Integer = 0
+    Private Const ICON_BIG As Integer = 1
+
+    Private Sub _LoadFaviconAsync(connection_info As ConnectionModel)
+        Task.Run(Sub() _LoadFavicon(connection_info))
+    End Sub
+
+    Private Sub _LoadFavicon(connection_info As ConnectionModel)
+
+        Dim address = connection_info.Address.ToLower()
+        If address.StartsWith("irc.") Then
+            address = address.Substring(4)
+        End If
+
+        Dim websiteUri = New Uri("http://" & address)
+
+        Dim source = New HttpSource() With {
+            .CachePolicy = New RequestCachePolicy(RequestCacheLevel.CacheIfAvailable),
+            .UserAgent = "QuickIRC"}
+        Dim fetcher = New Fetcher(source)
+
+        Using image = fetcher.FetchClosest(websiteUri, New Size(16, 16))
+            If image IsNot Nothing Then
+                Invoke(Sub() _SetIcon(image))
+            End If
+        End Using
+
+    End Sub
+
+    Private Sub _SetIcon(image As Image)
+        If Connection Is Nothing Then
+            Exit Sub
+        End If
+        Using bitmap = New Bitmap(image)
+            Dim smallIcon = Icon.FromHandle(bitmap.GetHicon())
+            SendMessage(Handle, WM_SETICON, ICON_SMALL, smallIcon.Handle)
+        End Using
     End Sub
 
 #End Region
